ffb0abbddc86e99b895e6763fbd1249a8c14c076
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-10-07 13:58:25 +0200

    Chore: Create ads package

---
+++ src/ads/__init__.py
@@ -0,0 +1,8 @@
+"""Algorithms and Data Structures implementations."""
+
+from ads.bst import BST
+from ads.deque import Deque
+from ads.hashtable import HashTableSC
+from ads.linked_list import LinkedList, LLNode
+
+__all__ = ["LinkedList", "LLNode", "Deque", "BST", "HashTableSC"]
